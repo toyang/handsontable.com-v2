bc36383aa554cc9f7f130bf89ba3ce77baec24d6
Bartek Bielawa <bartekbielawa@Barteks-iMac.local>
2015-12-14 21:11:45 +0100

    features

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+    var dir = {
+        css: 'www/static/css/',
+    };
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+       
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'www/static/css/main.css': [dir.css + '*.css']
+                }
+                
+            }
+        },
+
+        unused: {
+            options: {
+                reference: 'www/',
+                directory: ['**/*.ejs', '**/*.html', '**/*.css'],
+                remove: false // set to true to delete unused files from project 
+            }
+        },
+
+        clean: {
+            src: [dir.css + '*', "!www/**/*.css"]
+        },
+    });
+    
+    grunt.loadNpmTasks('grunt-unused');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', 'build');
+    grunt.registerTask('build', ['unused', 'cssmin', 'clean']);
+    
+};
