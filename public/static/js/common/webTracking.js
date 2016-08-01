b29cd6c0de22d92fb23cf80f900c351bb73e69b6
Krzysztof Budnik <budnix@gmail.com>
2016-08-01 11:35:53 +0200

    Update web tracking script

--- public/static/js/common/webTracking.js
+++ public/static/js/common/webTracking.js
@@ -13,8 +13,16 @@
   var additionalForm = document.querySelector('#mc_embed_free form');
 
   if (additionalForm) {
-    document.querySelector('#mc_embed_free form').addEventListener('submit', function() {
+    additionalForm.addEventListener('submit', function() {
       dataLayer.push({'event': 'Newsletter header subscribe'});
+    });
+  }
+
+  var contactForm = document.querySelector('#contact-form');
+
+  if (contactForm) {
+    contactForm.addEventListener('submit', function() {
+      dataLayer.push({'event': 'Contact form submit'});
     });
   }
 }());
