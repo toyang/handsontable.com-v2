e0b23faae7c4636305acb8f15d050872ab0de659
Marcin Kordacki <Marcin Kordacki>
2017-06-07 18:57:51 +0200

    First review udpate, On hold: pricing links, Aria support for svg's and support page which is going to get created, Rejected: aligment of section headers, To do: a non link space betweeb an icon and text. Additional fixes, Hot linking head section, checkboxes on example pages and contact, added additional button class ghost-white-cta for features header link

--- public/static/js/common/tabs.js
+++ public/static/js/common/tabs.js
@@ -22,6 +22,26 @@ function openDownload(evt, downloadName){
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
+function openService(evt, serviceName){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabset");
+
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("taburl");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+
+    document.getElementById(serviceName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 
 
 
